e30dbbf443c6d2594b1c0bb5b4b8d35df6785bcf
RAHUL576-R <rahul6344sharma@gmail.com>
2026-06-24 00:05:27 +0530

    d

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 import streamlit as st
-
-st.set_page_config(page_title="GW Filter", layout="wide")
-import os
-os.environ["STREAMLIT_SERVER_HEADLESS"] = "true"
 import torch
 import numpy as np
 from model import Autoencoder
 
 st.title("Gravitational Wave Noise Filter")
 
-# ── Load model ─────────────────────────────
 @st.cache_resource
 def load_model():
     model = Autoencoder()
@@ -21,13 +16,11 @@ def load_model():
 model = load_model()
 st.success("Model loaded successfully")
 
-# ── File upload ─────────────────────────────
 uploaded_file = st.file_uploader("Upload noisy signal (.npy)")
 
 if uploaded_file is not None:
     signal = np.load(uploaded_file)
 
-    # ── Convert to correct shape (B, 1, L) ──
     signal_tensor = torch.tensor(signal, dtype=torch.float32)
 
     if signal_tensor.ndim == 1:
@@ -35,11 +28,9 @@ if uploaded_file is not None:
     elif signal_tensor.ndim == 2:
         signal_tensor = signal_tensor.unsqueeze(1)
 
-    # ── Inference ─────────────────────────────
     with torch.no_grad():
         output = model(signal_tensor)
 
     output = output.squeeze().cpu().numpy()
 
-    st.write("Denoised Output:")
     st.line_chart(output)
